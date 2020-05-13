e6d7cf820c5601228bba1b0057f3c44623e60788
James Berg <james.berg38@yahoo.com>
2020-05-13 12:52:58 -0500

    Updates

---
+++ precache-manifest.bcd13fd7961655d09148073d642df587.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4717fc9096251790eae96b2ccc129946",
+    "url": "/magical-diary/index.html"
+  },
+  {
+    "revision": "5b49510e0d003f967c1d",
+    "url": "/magical-diary/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7715a9e7d6323e5878ae",
+    "url": "/magical-diary/static/js/2.de882fc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magical-diary/static/js/2.de882fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b49510e0d003f967c1d",
+    "url": "/magical-diary/static/js/main.c35c6409.chunk.js"
+  },
+  {
+    "revision": "a2a6aae3cd76b411ce34",
+    "url": "/magical-diary/static/js/runtime-main.68f56448.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/magical-diary/static/media/logo.5d5d9eef.svg"
+  }
+]);
